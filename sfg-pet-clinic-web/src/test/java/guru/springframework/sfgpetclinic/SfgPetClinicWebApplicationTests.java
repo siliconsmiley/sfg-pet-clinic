df70b3e688199d2c9efd79a517c5eddf42f851b1
Matthew Wagner <siliconsmiley@gmail.com>
2020-06-16 13:36:51 -0400

    Loaded sample data with command line runner. Closes #16

--- sfg-pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicWebApplicationTests.java
+++ sfg-pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicWebApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.sfgpetclinicweb;
+package guru.springframework.sfgpetclinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
