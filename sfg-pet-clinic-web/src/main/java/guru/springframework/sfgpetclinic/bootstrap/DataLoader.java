baa6d387a86d5755342d988234d49cefa74acf80
Matthew H. Wagner <siliconsmiley@gmail.com>
2020-06-18 16:35:22 -0400

    Added pet type data on startup.

--- sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ sfg-pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,41 +14,49 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        Owner owner1 = new Owner();
-        owner1.setFirstName("Michael");
-        owner1.setLastName("Weston");
-        ownerService.save(owner1);
-
-        Owner owner2 = new Owner();
-        owner2.setFirstName("Karen");
-        owner2.setLastName("CentralPark");
-        ownerService.save(owner2);
-
-        Owner owner3 = new Owner();
-        owner3.setFirstName("Joe");
-        owner3.setLastName("Dirt");
-        ownerService.save(owner3);
-
+        createOwner("Michael", "Weston");
+        createOwner("Karen", "CentralPark");
+        createOwner("Joe", "Dirt");
         System.out.println("loaded Owners...");
 
-        Vet vet1 = new Vet();
-        vet1.setFirstName("Sand");
-        vet1.setLastName("Witch");
-        vetService.save(vet1);
-
-        Vet vet2 = new Vet();
-        vet2.setFirstName("Silly");
-        vet2.setLastName("Vet");
-        vetService.save(vet2);
-
+        createVet("Sand", "Witch");
+        createVet("Silly", "Vet");
         System.out.println("loaded Vets...");
+
+        createPetType("Dog");
+        createPetType("Cat");
+        createPetType("Fish");
+        System.out.println("loaded Pet Types...");
+    }
+
+    private void createPetType(String name) {
+        PetType petType = new PetType();
+        petType.setName(name);
+        PetType savedPetType = petTypeService.save(petType);
+
+    }
+
+    private void createVet(String firstName, String lastName) {
+        Vet vet1 = new Vet();
+        vet1.setFirstName(firstName);
+        vet1.setLastName(lastName);
+        Vet savedVet = vetService.save(vet1);
+    }
+
+    private void createOwner(String firstName, String lastName) {
+        Owner owner1 = new Owner();
+        owner1.setFirstName(firstName);
+        owner1.setLastName(lastName);
+        Owner savedOwner = ownerService.save(owner1);
     }
 }
