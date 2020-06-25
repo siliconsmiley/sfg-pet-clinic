882e4a2948fc7d53bec0648629e49811cf7adeaf
Matthew H. Wagner <siliconsmiley@gmail.com>
2020-06-25 13:43:28 -0400

    Created spring data JPA service for owner. Fixed typo in pet type repository definition. Closes #42.

--- sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetTypeRepository.java
+++ sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetTypeRepository.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.repositories;
 
+import guru.springframework.sfgpetclinic.model.PetType;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PetTypeRepository extends CrudRepository<PetTypeRepository, Long> {
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
 }
