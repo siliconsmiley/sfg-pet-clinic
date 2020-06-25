ea8688cbbd9051e1c4ef6ee5fa0f782a69ecafaf
Matthew Wagner <siliconsmiley@gmail.com>
2020-06-25 12:36:10 -0400

    Annotated base entity for JPA data modelling. Closes #37.

--- sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ sfg-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,13 +1,21 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Simple JavaBean domain object with and id property. Used as a baseclass for objects that need this property.
+ *
  * @author MHW
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
